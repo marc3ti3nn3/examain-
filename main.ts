114609818c7f313e1194a000680c3c327666777f
marc3ti3nn3 <73190943+marc3ti3nn3@users.noreply.github.com>
2021-01-29 11:27:38 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,60 +9,83 @@ for (let index = 0; index < 4; index++) {
     basic.pause(500)
 }
 RobotCar_Keyestudio.Leds.ledsOff()
-for (let index = 0; index < 6; index++) {
-    if (randint(0, 5) == 0) {
-        for (let index = 0; index < 1; index++) {
-            RobotCar_Keyestudio.Motors.move(50)
-            basic.showArrow(ArrowNames.North)
-            basic.pause(1000)
-            RobotCar_Keyestudio.Motors.stop()
-            basic.pause(500)
-            RobotCar_Keyestudio.Motors.move(-50)
-            basic.showArrow(ArrowNames.South)
-            basic.pause(1000)
-            RobotCar_Keyestudio.Motors.stop()
-        }
-    } else if (randint(0, 5) == 1) {
-        for (let index = 0; index < 1; index++) {
-            music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
-            RobotCar_Keyestudio.Motors.spin(200)
-            basic.pause(5000)
-            RobotCar_Keyestudio.Motors.stop()
-        }
-    } else if (randint(0, 5) == 2) {
-        for (let index = 0; index < 4; index++) {
-            RobotCar_Keyestudio.Motors.spin(50)
-            basic.pause(1000)
-            RobotCar_Keyestudio.Motors.stop()
-            basic.pause(200)
-        }
-    } else if (randint(0, 5) == 3) {
-        for (let index = 0; index < 5; index++) {
-            RobotCar_Keyestudio.Leds.showRed()
-            music.playTone(494, music.beat(BeatFraction.Half))
-            basic.pause(500)
-            music.playTone(349, music.beat(BeatFraction.Half))
-            RobotCar_Keyestudio.Leds.showBlue()
-            basic.pause(500)
-        }
-    } else if (randint(0, 5) == 4) {
+for (let index = 0; index < 1; index++) {
+    RobotCar_Keyestudio.Motors.move(50)
+    basic.showArrow(ArrowNames.North)
+    basic.pause(1000)
+    RobotCar_Keyestudio.Motors.stop()
+    basic.pause(500)
+    RobotCar_Keyestudio.Motors.move(-50)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(1000)
+    RobotCar_Keyestudio.Motors.stop()
+    basic.clearScreen()
+}
+for (let index = 0; index < 1; index++) {
+    music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+    RobotCar_Keyestudio.Motors.spin(200)
+    basic.pause(5000)
+    RobotCar_Keyestudio.Motors.stop()
+}
+music.stopAllSounds()
+for (let index = 0; index < 4; index++) {
+    basic.pause(1000)
+    RobotCar_Keyestudio.Motors.steer(50, 50)
+    RobotCar_Keyestudio.Motors.stop()
+    basic.pause(200)
+}
+for (let index = 0; index < 5; index++) {
+    RobotCar_Keyestudio.Leds.showRed()
+    music.playTone(494, music.beat(BeatFraction.Half))
+    basic.pause(500)
+    music.playTone(349, music.beat(BeatFraction.Half))
+    RobotCar_Keyestudio.Leds.showBlue()
+    basic.pause(500)
+}
+RobotCar_Keyestudio.Leds.ledsOff()
+music.stopAllSounds()
+for (let index = 0; index < 3; index++) {
+    if (randint(1, 3) == 1) {
         basic.showLeds(`
-            # . . . #
             . . # . .
-            . . . . .
             . # # # .
-            # . . . #
+            # # # # #
+            . # # # .
+            . . # . .
             `)
-    } else if (randint(0, 5) == 5) {
+        basic.pause(2000)
+    } else if (randint(1, 3) == 2) {
         basic.showLeds(`
-            # . . . #
             . . # . .
-            . . . . .
-            # . . . #
+            # # # . .
+            # . # # #
+            # # . # .
             . # # # .
             `)
+        basic.pause(2000)
+    } else if (randint(1, 3) == 3) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(2000)
     }
 }
-basic.forever(function () {
-	
-})
+basic.clearScreen()
+basic.showString("Hello!")
+basic.pause(500)
+basic.showString("am")
+basic.pause(500)
+basic.showString("micro:bit")
+basic.pause(500)
+basic.clearScreen()
+if (input.temperature() < 15) {
+    basic.showIcon(IconNames.Sad)
+} else {
+    basic.showIcon(IconNames.Happy)
+}
+basic.pause(2000)
+basic.clearScreen()
